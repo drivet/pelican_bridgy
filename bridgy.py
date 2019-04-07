8833864508fa2b7fe32f8ec59b7c4254bbc73dd3
Desmond Rivet <desmond.rivet@gmail.com>
2019-04-07 12:38:07 -0400

    add code to support saving syndication urls

--- bridgy.py
+++ bridgy.py
@@ -1,7 +1,14 @@
 from pelican import signals
+
 from ronkyuu import sendWebmention
 import requests
+import os
+import base64
+import json
 
+
+WEBSITE = 'website'
+WEBSITE_CONTENTS = 'https://api.github.com/repos/drivet/' + WEBSITE + '/contents/content/'
 
 BRIDGY_ENDPOINT = r'https://brid.gy/publish/webmention'
 PUBLISH_TARGETS = [r'https://brid.gy/publish/twitter']
@@ -9,25 +16,30 @@ PUBLISH_TARGETS = [r'https://brid.gy/publish/twitter']
 syndicated_articles = []
 
 
+def fix_metadata(generator, metadata):
+    if 'mp_syndicate_to' not in metadata:
+        metadata['mp_syndicate_to'] = []
+    else:
+        metadata['mp_syndicate_to'] = metadata['mp_syndicate_to'].split(',')
+
+    if 'syndication' not in metadata:
+        metadata['syndication'] = []
+    else:
+        metadata['syndication'] = metadata['syndication'].split(',')
+
+
 def syndicate(generator):
     for article in list(generator.articles):
-        if not hasattr(article, 'mp_syndicate_to'):
-            article.mp_syndicate_to = ''
-
-        if not hasattr(article, 'syndication'):
-            article.syndication = ''
-
+        # skip if we do not want to syndicate, or we have already syndicated
         if not article.mp_syndicate_to or article.syndication:
             continue
 
-        article.syndication = []
-        for syndicate_target in [t for t in article.mp_syndicate_to.split(',')
-                                 if t in PUBLISH_TARGETS]:
+        for syndicate_target in [t for t in article.mp_syndicate_to if t in PUBLISH_TARGETS]:
             source_url = generator.settings['SITEURL'] + '/' + article.url
             if article.category == 'notes':
                 syndicate_target += '?bridgy_omit_link=true'
 
-            print('sending web mention from ' + source_url + " to " + syndicate_target + " using " + BRIDGY_ENDPOINT )
+            print('sending web mention from ' + source_url + " to " + syndicate_target + " using " + BRIDGY_ENDPOINT)
             r = sendWebmention(source_url, syndicate_target, BRIDGY_ENDPOINT)
             bridgy_response = r.json()
             if r.status_code == requests.codes.created:
@@ -35,26 +47,41 @@ def syndicate(generator):
             else:
                 print('Bridgy webmention failed with ' + str(r.status_code))
                 print('Error information ' + str(bridgy_response))
-        else:
-            print('no matching syndication targets')
 
         if article.syndication:
             syndicated_articles.append(article)
 
 
 def save_syndication(p):
-    print('calling save_syndication')
     for article in syndicated_articles:
-        print('opening ' + article.source_file)
-        with open(article.source_file, 'r') as f:
-            contents = f.read()
+        path = os.path.relpath(article.source_path, p.settings['PATH'])
+        url = WEBSITE_CONTENTS + path
+        fetch_request = requests.get(url, auth=(os.environ['USERNAME'], os.environ['PASSWORD']))
+
+        if fetch_request.status_code != 200:
+            raise Exception('failed to fetch ' + url + ' from github, code: ' + fetch_request.status_code)
+
+        response = fetch_request.json()
+        contents = b64decode(response['content'])
         pieces = contents.split('\n\n', 1)
-        new_contents = pieces[0] + '\nsyndication: ' + ','.join(syndicated_articles) + '\n' + pieces[1]
-        print('saving ' + article.source_file)
-        with open(article.source_file, 'w') as f:
-            f.write(new_contents)
+        new_contents = pieces[0] + '\nsyndication: ' + ','.join(article.syndication) + '\n\n' + pieces[1]
+        put_request = requests.put(url, auth=(os.environ['USERNAME'], os.environ['PASSWORD']),
+                                   data=json.dumps({'message': 'post to ' + path,
+                                                    'content': b64encode(new_contents),
+                                                    'sha': response['sha']}))
+        if put_request.status_code != 201:
+            raise Exception('failed to put article ' + url + ' on github, code: ' + put_request.status_code)
+
+
+def b64decode(s):
+    return base64.b64decode(s.encode()).decode()
+
+
+def b64encode(s):
+    return base64.b64encode(s.encode()).decode()
 
 
 def register():
+    signals.article_generator_context.connect(fix_metadata)
     signals.article_generator_finalized.connect(syndicate)
-    #signals.finalized.connect(save_syndication)
+    signals.finalized.connect(save_syndication)
